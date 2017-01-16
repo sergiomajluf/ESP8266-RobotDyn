849562159340a2ac29a61c208dff697e2ffd99de
Sergio Majluf <sergio.majluf@gmail.com>
2017-01-16 02:53:10 -0300

    first commit

--- esp-webServer/password-def.h
+++ esp-webServer/password-def.h
@@ -1,7 +1,7 @@
 /*
-Renombrar este archivo par que se llame
-"password.h"
-y modificar los datos de contacto de tu propia red
+	Renombrar este archivo para que se llame
+	"password.h" sin las comillas, claro ;)
+	y modificar los datos de acceso de tu propia wifi
 
 */
 const char* ssid = "nombre";
